d427e45c1b634ea875084c52ff9185e5af722aaa
H-M-Masud <hmmasud163@gmail.com>
2022-02-18 22:12:10 +0600

    default parameter

---
+++ let-const.js
@@ -0,0 +1,10 @@
+let balance = 1240;
+balance = 1340;
+const userName = "janpakhi";
+
+const numbers = [53, 534, 34, 353, 34];
+// numbers = [23, 45, 3543]; // not allowed resign
+
+for(let  i = 0; i <numbers.length; i++){
+    const number = numbers[i];
+}
