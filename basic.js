a92b81baf66316d8c9bc1fa46cb9b73f4185c457
H-M-Masud <hmmasud163@gmail.com>
2022-02-18 21:32:18 +0600

    recap basic js

---
+++ basic.js
@@ -0,0 +1,58 @@
+// variable 
+var deposit = 400;
+// condition
+if(deposit > 500){
+
+}
+else if(deposit < 200){
+
+}
+else if(deposit == 500){
+
+}
+else if(deposit !=500){
+
+}
+else if(deposit >=500){
+
+}
+else if(deposit <= 500){
+
+}
+else{
+
+}
+
+// array
+const numbers = [3,34, 43, 53, 34, 43, 45, 23,34];
+const numberCount = numbers.length;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 555;
+// check whether 222 included in the array
+if(numbers.indexOf(222) != -1){
+
+}
+if(numbers.includes(222)){
+
+}
+
+// loop
+// while, for
+for(const number of numbers){
+
+}
+
+// function
+function fullName(first, second){
+    const name = first + ' '+ second;
+    return name;
+}
+const person = fullName('rahim', 'ali');
+
+// object
+const bottle = {
+    color: 'yellow',
+    contain: 'water',
+    price: 50
+}
