4f2eee948a033df4fe4be5ca362a8dd65667fb88
H-M-Masud <hmmasud163@gmail.com>
2022-02-18 22:47:34 +0600

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,16 @@
+// function declare
+function add(num1, num2){
+    return num1 + num2;
+}
+
+// function expression
+const add2 = function add2(num1, num2){
+    return num1 + num2;
+}
+// function expession(anonymous)
+const add3 = function (num1, num3){
+    return num1 + num2;
+}
+
+// arrow function
+const add4 = (num1, num2) => num1 + num2;
